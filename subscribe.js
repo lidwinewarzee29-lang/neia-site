312925b7087bb6dadbcf09c8d018ae4c570c4aa1
lidwinewarzee29-lang <lidwinewarzee.29@gmail.com>
2026-06-29 10:54:01 +0200

    Add files via upload

---
+++ subscribe.js
@@ -0,0 +1,31 @@
+exports.handler = async function(event) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const { email, listIds, updateEnabled } = JSON.parse(event.body);
+
+    const response = await fetch('https://api.brevo.com/v3/contacts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'api-key': process.env.BREVO_API_KEY
+      },
+      body: JSON.stringify({ email, listIds, updateEnabled })
+    });
+
+    const data = response.status === 204 ? {} : await response.json();
+
+    return {
+      statusCode: response.status,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Erreur serveur' })
+    };
+  }
+};
